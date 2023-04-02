be03cb655860a76c40a06c1545e74d816d9e7bec
Almortagel <105941198+Almortage@users.noreply.github.com>
2023-04-02 12:12:46 +0200

    Update __main__.py

--- jmub/__main__.py
+++ jmub/__main__.py
@@ -19,7 +19,7 @@ from .utils import (
     verifyLoggerGroup,
 )
 
-LOGS = logging.getLogger("سورس جمثون")
+LOGS = logging.getLogger("سورس افاتار")
 
 cmdhr = Config.COMMAND_HAND_LER
 
@@ -51,7 +51,7 @@ except Exception as meo:
 
 async def startup_process():
     if jmub.uid in blacklisted_users:
-        LOGS.info("انت لا يمكنك تنصيب سورس جمثون عزيزي دي")
+        LOGS.info("انت لا يمكنك تنصيب سورس جمثون افاتار عزيزي دي")
         return
     if not gvarstatus("TNSEEB"):
         try:
@@ -62,7 +62,7 @@ async def startup_process():
             LOGS.info("تم انتهاء عملية التنصيب بنجاح")
             LOGS.info(
                 f"لمعرفة اوامر السورس ارسل {cmdhr}الاوامر\
-                \nمجموعة قناة السورس  https://t.me/jmthon_support"
+                \nمجموعة قناة السورس  https://t.me/va_source"
             )
             LOGS.info("============================================================")
             await verifyLoggerGroup()
@@ -75,9 +75,9 @@ async def startup_process():
             LOGS.info(str(e))
             return
     else:
-        LOGS.info("انت لا يمكنك تنصيب سورس جمثون عزيزي دي")
-        LOGS.info("انت لا يمكنك تنصيب سورس جمثون عزيزي دي")
-        LOGS.info("انت لا يمكنك تنصيب سورس جمثون عزيزي دي")
+        LOGS.info("انت لا يمكنك تنصيب سورس جمثون افاتار عزيزي دي")
+        LOGS.info("انت لا يمكنك تنصيب سورس جمثون افاتار عزيزي دي")
+        LOGS.info("انت لا يمكنك تنصيب سورس جمثون افاتار عزيزي دي")
 
 
 jmub.loop.run_until_complete(startup_process())
