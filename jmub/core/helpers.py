382a8b7365163475f01bb9659bfc57e9e4f82cc9
Almortagel <105941198+Almortage@users.noreply.github.com>
2023-04-03 04:30:24 +0200

    Update helpers.py

--- jmub/core/helpers.py
+++ jmub/core/helpers.py
@@ -6,7 +6,7 @@ from telethon.utils import get_display_name
 
 from .events import NewMessage
 
-LOGGER = logging.getLogger("سورس جمثون")
+LOGGER = logging.getLogger("سورس افاتار")
 
 
 def printUser(entity: types.User) -> None:
