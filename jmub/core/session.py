9b67b18230b7d338f5ef779d46ae384b92e7a78d
Almortagel <105941198+Almortage@users.noreply.github.com>
2023-04-03 04:29:08 +0200

    Update session.py

--- jmub/core/session.py
+++ jmub/core/session.py
@@ -7,7 +7,7 @@ from .bothseesion import bothseesion
 from .client import JmthonClient
 from .logger import logging
 
-LOGS = logging.getLogger("سورس جمثون")
+LOGS = logging.getLogger("سورس افاتار")
 __version__ = "2.10.6"
 
 loop = None
@@ -15,7 +15,7 @@ loop = None
 if Config.STRING_SESSION:
     session = bothseesion(Config.STRING_SESSION, LOGS)
 else:
-    session = "jmthon"
+    session = "AVATAR"
 
 try:
     jmub = JmthonClient(
@@ -54,7 +54,7 @@ except Exception as e:
 
 
 jmub.tgbot = tgbot = JmthonClient(
-    session="jmthonTgbot",
+    session="Almortagelbot",
     api_id=Config.APP_ID,
     api_hash=Config.API_HASH,
     loop=loop,
