45059c4163a8bec0c4954e4e790f10218637cb12
Almortagel <105941198+Almortage@users.noreply.github.com>
2023-09-06 21:20:05 +0300

    Add files via upload

--- jmub/helpers/functions/ialivetext.py
+++ jmub/helpers/functions/ialivetext.py
@@ -82,7 +82,7 @@ async def jmthonalive(StartTime):
         dyno = f"{AppHours}h {AppMinutes}m/{hours}h {minutes}m"
     except Exception as e:
         dyno = e
-    return f"🖤༄ سورس جمثون ༄🖤\
+    return f"🖤༄ سورس المرتجل ༄🖤\
                  \n\nღ ثاعدة البيانات : {check_sgnirts}\
                   \nღ المطور الثانوي : {sudo}\
                   \nღ الوقت : {uptime}\
